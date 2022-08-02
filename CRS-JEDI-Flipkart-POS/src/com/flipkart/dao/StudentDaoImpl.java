bd8d67203c6bfaa38c8452aaa14b9f1525aaf442
kushalagarwal25 <kushalagarwal25@gmail.com>
2022-08-02 10:17:37 +0530

    added some dao methods

--- CRS-JEDI-Flipkart-POS/src/com/flipkart/dao/StudentDaoImpl.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/dao/StudentDaoImpl.java
@@ -1,4 +1,22 @@
 package com.flipkart.dao;
 
+import com.flipkart.utils.DBUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class StudentDaoImpl implements StudentDaoInterface {
+    private static volatile StudentDaoImpl instance = null;
+    private PreparedStatement statement = null;
+    Connection connection = DBUtils.getConnection();
+    private StudentDaoImpl() {}
+
+    public static StudentDaoImpl getInstance(){
+        if(instance == null) {
+            synchronized(StudentDaoImpl.class) {
+                instance = new StudentDaoImpl();
+            }
+        }
+        return instance;
+    }
 }
--- CRS-JEDI-Flipkart-POS/src/com/flipkart/service/AdminServiceImpl.java
+++ CRS-JEDI-Flipkart-POS/src/com/flipkart/service/AdminServiceImpl.java
@@ -4,6 +4,8 @@ import com.flipkart.bean.Catalog;
 import com.flipkart.bean.Course;
 import com.flipkart.bean.GradeCard;
 import com.flipkart.bean.Student;
+import com.flipkart.dao.AdminDaoImpl;
+import com.flipkart.dao.AdminDaoInterface;
 import com.flipkart.dao.DummyData;
 
 import java.util.ArrayList;
@@ -42,12 +44,23 @@ public class AdminServiceImpl implements AdminInterface{
 
     @Override
     public GradeCard generateGradeCard() {
-        return null;
+
+
     }
 
     @Override
     public void addCourse() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter course ID: ");
+        int courseId = scanner.nextInt();
+        System.out.print("Enter course name: ");
+        String courseName = scanner.nextLine();
+        System.out.print("Enter instructor's name: ");
+        String instructor = scanner.nextLine();
+        Course course = new Course(courseId,courseName,true,instructor);
 
+        AdminDaoInterface admin = new AdminDaoImpl();
+        admin.addCourse(course);
     }
 
     @Override
@@ -56,18 +69,20 @@ public class AdminServiceImpl implements AdminInterface{
         courses.forEach((course) -> System.out.println(course.getCourseId() + " " + course.getCourseName()));
         System.out.print("Choose a course to remove: ");
         Scanner scanner = new Scanner((System.in));
-        String choice = scanner.nextLine();
-        List<Course> updateCourses = new ArrayList<Course>();
-        courses.forEach((course) -> {
-            if(!Objects.equals(course.getCourseId(),choice)) {
-                updateCourses.add(course);
-            }
-        });
-        Catalog cat = new Catalog();
-        cat.courseList = updateCourses;
-        data.catalog.put(1,cat);
-        courses = data.catalog.get(1).courseList;
-        courses.forEach((course) -> System.out.println(course.getCourseId() + " " + course.getCourseName()));
-        System.out.println("Course "+choice+" removed successfully!");
+        String courseId = scanner.nextLine();
+        AdminDaoInterface admin = new AdminDaoImpl();
+        admin.removeCourse(courseId);
+//        List<Course> updateCourses = new ArrayList<Course>();
+//        courses.forEach((course) -> {
+//            if(!Objects.equals(course.getCourseId(),choice)) {
+//                updateCourses.add(course);
+//            }
+//        });
+//        Catalog cat = new Catalog();
+//        cat.courseList = updateCourses;
+//        data.catalog.put(1,cat);
+//        courses = data.catalog.get(1).courseList;
+//        courses.forEach((course) -> System.out.println(course.getCourseId() + " " + course.getCourseName()));
+//        System.out.println("Course "+choice+" removed successfully!");
     }
 }
