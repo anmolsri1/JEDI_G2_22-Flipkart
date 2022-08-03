a7ead887d8a9d474f545ee53812963932750d964
Anmol Srivastava <anmol.srivastava1@flipkart.com>
2022-08-03 15:26:37 +0530

    add Dropwizard

---
+++ CRS-G2-Flipkart-Dropwizard/src/main/java/com/flipkart/utils/DBUtils.java
@@ -0,0 +1,36 @@
+package com.flipkart.utils;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class DBUtils {
+	private static Connection connection = null;
+	public static Connection getConnection() {
+        if (connection != null) {
+        	try {
+        		if (connection.isClosed()) {
+//                	System.out.println("Connection closed!");
+                	connection = null;
+                	return getConnection();
+                } else {
+//                	System.out.println("Connection working!");
+                	return connection;
+                }
+        	} catch (SQLException e) {
+        		System.out.println("Error: " + e.getMessage());
+        		return getConnection();
+        	}
+        } else {
+            try {
+                String url = "jdbc:mysql://localhost:3306/CRSDATABASE";
+                String user = "root";
+                String password = "postgres";
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(url, user, password);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+            return connection;
+        }
+    }
+}
